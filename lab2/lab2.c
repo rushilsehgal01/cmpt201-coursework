bde500a64c9efc4767d53918148d40f53e17a56a
Rushil Sehgal <rsa203@sfu.ca>
2026-09-21 14:28:57 -0700

    lab1 done

---
+++ lab2/lab2.c
@@ -0,0 +1,12 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+  while (true) {
+    printf("Enter programs to run. \n");
+    printf("> ");
+  }
+}
